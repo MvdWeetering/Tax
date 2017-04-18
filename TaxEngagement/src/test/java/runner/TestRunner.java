2a27f731ee891ca0f28a15805d6a5a96f393db50
mvdweetering <mvdweetering@TAXTEST01.work.local>
2017-04-18 16:20:07 +0200

    update runner

--- TaxEngagement/src/test/java/runner/TestRunner.java
+++ TaxEngagement/src/test/java/runner/TestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         format = { "pretty", "html:target/Reports/html/" },
         glue = {"steps"},
         features = "classpath:featurefiles/",
-        tags = {"@Toelichting_Balans"}
+        tags = {"@vullen_formulier"}
        
 )
 public class TestRunner {
