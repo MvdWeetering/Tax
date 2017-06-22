7b0b378dbd7c9baf74bb429f4513771c3b5a5e45
mvdweetering <mvdweetering@LT0005.work.local>
2017-06-22 07:45:44 +0200

    fiscale vermogensvergelijking compleet gemaakt

---
+++ TaxEngagement/src/main/java/codebase/FiscaleVermogensvergelijkingXLS.java
@@ -0,0 +1,128 @@
+package codebase;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class FiscaleVermogensvergelijkingXLS {
+	private static XSSFWorkbook wb;
+
+	
+	public static String HaalText(String Kolom, int Rij, String Tab)  {
+		
+		String resultaat = null;
+		int Column=0;
+		
+		try {
+			// Open the Excel file
+			FileInputStream fis = new FileInputStream("C:\\testdata\\fiscale vermogensvergelijking.xlsx");
+			
+			wb = new XSSFWorkbook(fis);
+			XSSFSheet sheet = wb.getSheet(Tab);
+			XSSFRow row = sheet.getRow(Rij-1);
+			
+			if (Kolom.equals("A")) {
+				Column = 0;
+			}
+			if (Kolom.equals("B")) {
+				Column = 1;
+			}
+			if (Kolom.equals("C")) {
+				Column = 2;
+			}
+			if (Kolom.equals("D")) {
+				Column = 3;
+			}
+			if (Kolom.equals("E")) {
+				Column = 4;
+			}
+			if (Kolom.equals("F")) {
+				Column = 5;
+			}
+			if (Kolom.equals("G")) {
+				Column = 6;
+			}
+			if (Kolom.equals("H")) {
+				Column = 7;
+			}
+			
+			resultaat =String.valueOf((row.getCell(Column).getStringCellValue()));		
+						
+			fis.close();
+		} catch (IOException e) {
+			return "Test data file not found";
+		}
+		catch (NullPointerException f) {
+			return "Cell has no value";
+		}
+		catch (IllegalStateException g) {
+			return "a";
+		}
+		
+		return resultaat;
+}
+	
+	public static String HaalData(String Kolom, int Rij, String Tab) {
+		
+		String resultaat = null;
+		int Column=0;
+		
+		try {
+			// Open the Excel file
+			FileInputStream fis = new FileInputStream("C:\\testdata\\fiscale vermogensvergelijking.xlsx");
+			
+			wb = new XSSFWorkbook(fis);
+			XSSFSheet sheet = wb.getSheet(Tab);
+			XSSFRow row = sheet.getRow(Rij-1);
+			
+			if (Kolom.equals("A")) {
+				Column = 0;
+			}
+			if (Kolom.equals("B")) {
+				Column = 1;
+			}
+			if (Kolom.equals("C")) {
+				Column = 2;
+			}
+			if (Kolom.equals("D")) {
+				Column = 3;
+			}
+			if (Kolom.equals("E")) {
+				Column = 4;
+			}
+			if (Kolom.equals("F")) {
+				Column = 5;
+			}
+			if (Kolom.equals("G")) {
+				Column = 6;
+			}
+			if (Kolom.equals("H")) {
+				Column = 7;
+			}
+			
+			resultaat =String.valueOf((row.getCell(Column).getNumericCellValue()));		
+						
+			fis.close();
+		} catch (IOException e) {
+			return "Test data file not found";
+		}
+		catch (NullPointerException f) {
+			return "Cell has no value";
+		}
+		catch (IllegalStateException g) {
+			return "a";
+		}
+		
+		return resultaat;
+	}	
+
+	public static void main(String[] args) {
+
+	System.out.println(FiscaleVermogensvergelijkingXLS.HaalData("E", 5, "TC01"));
+
+	}
+}
---
+++ TaxEngagement/src/main/java/pageObjects/FiscaleVermogensVergelijkingObjecten.java
@@ -0,0 +1,280 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class FiscaleVermogensVergelijkingObjecten {
+	private static WebElement element = null;
+	
+	@FieldName(name = "OndernemingsvermogenEindeJaar")
+	public static WebElement OndernemingsvermogenEindeJaar (WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzu4"));
+		return element;
+	}
+	
+	@FieldName(name = "MutatiesKapitaal")
+	public static WebElement MutatiesKapitaal (WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzu5"));
+		return element;
+	}
+	
+	@FieldName(name = "TotaalEindvermogenTerugbetalingen ")
+	public static WebElement TotaalEindvermogenTerugbetalingen(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzu7"));
+		return element;
+	}
+	
+	@FieldName(name = "OndernemingsvermogenBeginJaar")
+	public static WebElement OndernemingsvermogenBeginJaar(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzu8"));
+		return element;
+	}
+	
+	@FieldName(name = "StortingKapitaal ")
+	public static WebElement StortingKapitaal(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzua"));
+		return element;
+	}
+	
+	@FieldName(name = "TotaalBeginvermogenStortingen ")
+	public static WebElement TotaalBeginvermogenStortingen(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzub"));
+		return element;
+	}
+	
+	@FieldName(name = "Vermogensverschil")
+	public static WebElement Vermogensverschil(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzuc"));
+		return element;
+	}
+	
+	@FieldName(name = "NietAftrekbareBedragen ")
+	public static WebElement NietAftrekbareBedragen(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzud"));
+		return element;
+	}
+	
+	@FieldName(name = "SaldoFiscaleWinstberekening ")
+	public static WebElement SaldoFiscaleWinstberekening(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzuf"));
+		return element;
+	}
+	
+	@FieldName(name = "ResultaatWinstVerliesrekening ")
+	public static WebElement ResultaatWinstVerliesrekening(WebDriver driver){
+		element = driver.findElement(By.id("idresWinVer"));
+		return element;
+	}
+	
+	@FieldName(name = "Verschil")
+	public static WebElement Verschil(WebDriver driver){
+		element = driver.findElement(By.id("idverschil"));
+		return element;
+	}
+	
+	@FieldName(name = "DividendOnderworpenWinstuitdelingen")
+	public static WebElement DividendOnderworpenWinstuitdelingen(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tze9"));
+		return element;
+	}
+	
+	@FieldName(name = "OmschrijvingUitdeling_1")
+	public static WebElement OmschrijvingUitdeling_1(WebDriver driver){
+		element = driver.findElement(By.id("idtableAanDivOndWin:0-0"));
+		return element;
+	}
+	
+	@FieldName(name = "DatumTerbeschikkingstelling_1")
+	public static WebElement DatumTerbeschikkingstelling_1(WebDriver driver){
+		element = driver.findElement(By.id("idtableAanDivOndWin:0-1"));
+		return element;
+	}
+	
+	@FieldName(name = "DatumAangifteDividendbelasting_1")
+	public static WebElement DatumAangifteDividendbelasting_1(WebDriver driver){
+		element = driver.findElement(By.id("idtableAanDivOndWin:0-3"));
+		return element;
+	}
+	
+	@FieldName(name = "BedragIngehoudenDividendbelasting_1")
+	public static WebElement BedragIngehoudenDividendbelasting_1(WebDriver driver){
+		element = driver.findElement(By.id("idtableAanDivOndWin:0-5"));
+		return element;
+	}
+	
+	@FieldName(name = "BedragWinstuitdeling_1")
+	public static WebElement BedragWinstuitdeling_1(WebDriver driver){
+		element = driver.findElement(By.id("idtableAanDivOndWin:0-6"));
+		return element;
+	}
+	
+	@FieldName(name = "OmschrijvingUitdeling_2")
+	public static WebElement OmschrijvingUitdeling_2(WebDriver driver){
+		element = driver.findElement(By.id("idtableAanDivOndWin:1-0"));
+		return element;
+	}
+	
+	@FieldName(name = "DatumTerbeschikkingstelling_2")
+	public static WebElement DatumTerbeschikkingstelling_2(WebDriver driver){
+		element = driver.findElement(By.id("idtableAanDivOndWin:1-1"));
+		return element;
+	}
+	
+	@FieldName(name = "DatumAangifteDividendbelasting_2")
+	public static WebElement DatumAangifteDividendbelasting_2(WebDriver driver){
+		element = driver.findElement(By.id("idtableAanDivOndWin:1-3"));
+		return element;
+	}
+	
+	@FieldName(name = "BedragIngehoudenDividendbelasting_2")
+	public static WebElement BedragIngehoudenDividendbelasting_2(WebDriver driver){
+		element = driver.findElement(By.id("idtableAanDivOndWin:1-5"));
+		return element;
+	}
+	
+	@FieldName(name = "BedragWinstuitdeling_2")
+	public static WebElement BedragWinstuitdeling_2(WebDriver driver){
+		element = driver.findElement(By.id("idtableAanDivOndWin:1-6"));
+		return element;
+	}
+	
+	@FieldName(name = "VennootschapsbelastingFiscaalVermogenOnttrokken")
+	public static WebElement VennootschapsbelastingFiscaalVermogenOnttrokken(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzeb"));
+		return element;
+	}
+	
+	@FieldName(name = "BuitenlandseBelastingRegeling")
+	public static WebElement BuitenlandseBelastingRegeling(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzec"));
+		return element;
+	}
+	
+	@FieldName(name = "NietAftrekbaarWinstuitdelingenCooperaties ")
+	public static WebElement NietAftrekbaarWinstuitdelingenCooperaties(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzed"));
+		return element;
+	}
+	
+	@FieldName(name = "WinstuitdelingenCooperaties")
+	public static WebElement WinstuitdelingenCooperaties(WebDriver driver){
+		element = driver.findElement(By.id("idcellWinDooCoo"));
+		return element;
+	}
+	
+	@FieldName(name = "AftrekbaarDeelWinstuitdelingenCooperaties")
+	public static WebElement AftrekbaarDeelWinstuitdelingenCooperaties(WebDriver driver){
+		element = driver.findElement(By.id("idcellAftDeeWinDooCoo"));
+		return element;
+	}
+	
+	@FieldName(name = "NietAftrekbaarWinstuitdelingenCooperaties_sub")
+	public static WebElement NietAftrekbaarWinstuitdelingenCooperaties_sub(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzed"));
+		return element;
+	}
+	
+	@FieldName(name = "AndereOpenlijkeVermomdeUitdelingenWinst")
+	public static WebElement AndereOpenlijkeVermomdeUitdelingenWinst(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzef"));
+		return element;
+	}
+	
+	@FieldName(name = "NietAftrekbaarDeelBeloningenCommissarissen")
+	public static WebElement NietAftrekbaarDeelBeloningenCommissarissen(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzeh"));
+		return element;
+	}
+	
+	@FieldName(name = "BeloningenCommissarissen")
+	public static WebElement BeloningenCommissarissen(WebDriver driver){
+		element = driver.findElement(By.id("idcellBelCom"));
+		return element;
+	}
+	
+	@FieldName(name = "AftrekbaarDeelBeloningenCommissarissen ")
+	public static WebElement AftrekbaarDeelBeloningenCommissarissen(WebDriver driver){
+		element = driver.findElement(By.id("idcellAftDeeBelCom"));
+		return element;
+	}
+	
+	@FieldName(name = "NietAftrekbaarDeelBeloningenCommissarissen_sub")
+	public static WebElement NietAftrekbaarDeelBeloningenCommissarissen_sub(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzeh"));
+		return element;
+	}
+	
+	@FieldName(name = "NietAftrekbaarDeelTantiemes")
+	public static WebElement NietAftrekbaarDeelTantiemes(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzek"));
+		return element;
+	}
+	
+	@FieldName(name = "Tantiemes")
+	public static WebElement Tantiemes(WebDriver driver){
+		element = driver.findElement(By.id("idcellTan"));
+		return element;
+	}
+	
+	@FieldName(name = "AftrekbaarDeelTantiemes")
+	public static WebElement AftrekbaarDeelTantiemes(WebDriver driver){
+		element = driver.findElement(By.id("idcellAftDeeTan"));
+		return element;
+	}
+	
+	@FieldName(name = "NietAftrekbaarDeelTantiemes_sub")
+	public static WebElement NietAftrekbaarDeelTantiemes_sub(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzek"));
+		return element;
+	}
+	
+	@FieldName(name = "NietAftrekbaarDeelVergoedingenKapitaalverstrekking")
+	public static WebElement NietAftrekbaarDeelVergoedingenKapitaalverstrekking(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzeo"));
+		return element;
+	}
+	
+	@FieldName(name = "VergoedingenVoorKapitaalverstrekking")
+	public static WebElement VergoedingenVoorKapitaalverstrekking(WebDriver driver){
+		element = driver.findElement(By.id("idcellVerVooKap"));
+		return element;
+	}
+	
+	@FieldName(name = "AftrekbaarDeelVergoedingenVooKapitaalverstrekking")
+	public static WebElement AftrekbaarDeelVergoedingenVooKapitaalverstrekking(WebDriver driver){
+		element = driver.findElement(By.id("idcellAftDeeVerVooKap"));
+		return element;
+	}
+	
+	@FieldName(name = "NietAftrekbaarDeelVergoedingenKapitaalverstrekking_sub")
+	public static WebElement NietAftrekbaarDeelVergoedingenKapitaalverstrekking_sub(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzeo"));
+		return element;
+	}
+	
+	@FieldName(name = "UitkeringenIngevolgeStatutaireAndereVoorschriften")
+	public static WebElement UitkeringenIngevolgeStatutaireAndereVoorschriften(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzer"));
+		return element;
+	}
+	
+	@FieldName(name = "ResultaatTijdelijkIngekochteAandelenWerknemeropties")
+	public static WebElement ResultaatTijdelijkIngekochteAandelenWerknemeropties(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzeu"));
+		return element;
+	}
+	
+	@FieldName(name = "KostenAankoopResterendeAandelenDochtermaatschappijen")
+	public static WebElement KostenAankoopResterendeAandelenDochtermaatschappijen(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzev"));
+		return element;
+	}	
+	
+	@FieldName(name = "NietAftrekbareBedragen_Tot")
+	public static WebElement NietAftrekbareBedragen_Tot(WebDriver driver){
+		element = driver.findElement(By.id("idxdt9tzud"));
+		return element;
+	}
+	
+}
